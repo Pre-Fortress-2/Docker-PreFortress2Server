e3ae88c802769304f0a9f167b93cc71da5521aff
dani <35577693+dangreene0@users.noreply.github.com>
2022-11-24 20:43:24 -0500

    added actual update logic

--- scripts/pf-update.sh
+++ scripts/pf-update.sh
@@ -1,35 +1,59 @@
 #!/usr/bin/env bash
 # Updates Pre-Fortress 2.
 
-echo "Downloading game."
-cd sdk
-
-# Downloads all .7z files from the latest release.
-curl -s https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest \
-| grep "browser_download_url.*7z" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
-
-# Removes all .7z files that isn't the full game.
-for FILE in *
-do
-	if [[ $FILE == *"full"* ]]
-	then
-  		PF2=$FILE
-	else
-		if [[ $FILE == *".7z"* ]]
-		then
-			rm -f $FILE
-		f [[ $FILE == *".7z."* ]]
-		then
-			rm -f $FILE
-		fi
+INSTALL=./sdk/pf2/
+if test -f "$INSTALL"; then
+    echo "No PF2 Installation exists."
+    download_game
+    if test -f "currentVersion.txt"; then
+    	content=$(curl -L https://raw.githubusercontent.com/Pre-Fortress-2/pf2/main/currentVersion.txt)
+    	if grep -Fxq "$content" currentVersion.txt; then
+    	    echo "Matching version, no need to update."
+	else:
+	    echo "Downloading update."
+	    curl -L https://raw.githubusercontent.com/Pre-Fortress-2/pf2/main/currentVersion.txt > ./sdk/currentVersion.txt
+	    download_game
 	fi
-done
+    else:
+    	echo "No verbose version text detected."
+	curl -L https://raw.githubusercontent.com/Pre-Fortress-2/pf2/main/currentVersion.txt -o ./sdk/currentVersion.txt
+	download_game
+    fi
+    	
+fi
 
-7za x -y $PF2
-cd ..
+download_game() {
+	echo "Downloading game."
+	cd sdk
+
+	# Downloads all .7z files from the latest release.
+	curl -s https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest \
+	| grep "browser_download_url.*7z" \
+	| cut -d : -f 2,3 \
+	| tr -d \" \
+	| wget -qi -
+
+	# Removes all .7z files that isn't the full game.
+	for FILE in *
+	do
+		if [[ $FILE == *"full"* ]]
+		then
+			PF2=$FILE
+		else
+			if [[ $FILE == *".7z"* ]]
+			then
+				rm -f $FILE
+			f [[ $FILE == *".7z."* ]]
+			then
+				rm -f $FILE
+			fi
+		fi
+	done
+
+	7za x -y $PF2
+	cd ..
+
+}
 
 echo "Removing redundant C++ binary"
 rm ./sdk/bin/libstdc++.so.6
