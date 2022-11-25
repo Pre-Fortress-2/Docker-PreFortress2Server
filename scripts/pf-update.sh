e7d8f432b1f4d973be4cb4d0ee24a165805e4ee2
dani <35577693+dangreene0@users.noreply.github.com>
2022-11-25 00:08:38 -0500

    removed ls commands

--- scripts/pf-update.sh
+++ scripts/pf-update.sh
@@ -4,7 +4,7 @@
 download_game() {
 	echo "Downloading game."
 	cd sdk
-	ls
+	
 	# Downloads all .7z files from the latest release.
 	curl -s https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest \
 	| grep "browser_download_url.*7z" \
@@ -30,9 +30,8 @@ download_game() {
 	7za x -y $PF2
 	cd ..
 }
-ls
-INSTALL=./sdk/pf2/
 
+INSTALL=./sdk/pf2/
 if [ -d "$INSTALL" ]; then
 	if test -f "./sdk/currentVersion.txt"; then
 		content=$(curl -L https://raw.githubusercontent.com/Pre-Fortress-2/pf2/main/currentVersion.txt)
