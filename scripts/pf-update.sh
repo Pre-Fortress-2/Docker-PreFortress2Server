e8782e46571444741103e17d94820abadcbd8538
dangreene0 <35577693+dangreene0@users.noreply.github.com>
2022-11-24 22:29:00 -0500

    testing directories

--- scripts/pf-update.sh
+++ scripts/pf-update.sh
@@ -4,7 +4,7 @@
 download_game() {
 	echo "Downloading game."
 	cd sdk
-
+	ls
 	# Downloads all .7z files from the latest release.
 	curl -s https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest \
 	| grep "browser_download_url.*7z" \
@@ -34,7 +34,7 @@ ls
 INSTALL=./sdk/pf2/
 
 if [ -d "$INSTALL" ]; then
-	if test -f "currentVersion.txt"; then
+	if test -f "./sdk/currentVersion.txt"; then
 		content=$(curl -L https://raw.githubusercontent.com/Pre-Fortress-2/pf2/main/currentVersion.txt)
 		if grep -Fxq "$content" currentVersion.txt; then
 			echo "Matching version, no need to update."
