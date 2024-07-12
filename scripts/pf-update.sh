ca0ade7a72c608536e507a9e5e8c643dfdf90240
dani <dangreene0@gmail.com>
2024-07-12 04:40:24 +0000

    Update pf-update.sh
    - Changed link to archive
    - Removed complex tag logic
    - Simplified script

--- scripts/pf-update.sh
+++ scripts/pf-update.sh
@@ -3,58 +3,36 @@
 
 download_game() {
 	echo "Downloading game."
+
 	cd sdk
-
+	curl https://archive.prefortress.com/latest/version.txt > ./version.txt
+	
 	echo "Removing any leftover game files."
-	rm -f *.7z
+	rm -f latest.tar.gz
 
-	# Downloads all .7z files from the latest release.
-	curl -s https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest \
-	| grep "browser_download_url.*7z" \
-	| cut -d : -f 2,3 \
-	| tr -d \" \
-	| wget -qi -
+	# Downloads latest tarball from the latest archive.
+	wget https://archive.prefortress.com/latest/latest.tar.gz
 
-	# Removes all .7z files that isn't the full game.
-	for FILE in *
-	do
-		if [[ $FILE == *"full"* ]]; then
-			PF2=$FILE
-		else
-			if [[ $FILE == *".7z"* ]]; then
-				rm -f $FILE
-			fi
-		fi
-	done
-
-	7za x -y $PF2
+	tar -xzf latest.tar.gz
 	cd ..
 }
-
-
-# this needs to be updated, its reliant on the old currentVersion.txt system.
-# needs to just check the tag from the latest release
-# curl https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest -s | jq .tag_name -r
 
 INSTALL=./sdk/pf2/
 if [ -d "$INSTALL" ]; then
 	if test -f "./sdk/currentVersion.txt"; then
-		content=$(curl https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest -s | jq .tag_name -r)
-		if grep -Fxq "$content" ./sdk/currentVersion.txt; then
+		content=$(curl https://archive.prefortress.com/latest/version.txt)
+		if grep -Fxq "$content" ./sdk/version.txt; then
 			echo "Matching version, no need to update."
 		else
-			echo "Downloading update."
-			curl https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest -s | jq .tag_name -r > ./sdk/currentVersion.txt
+			echo "Update Detected."
 			download_game
 		fi
 	else
 		echo "No verbose version text detected."
-		curl https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest -s | jq .tag_name -r > ./sdk/currentVersion.txt
 		download_game
     fi
 else
 	echo "No PF2 Installation exists."
-	curl https://api.github.com/repos/Pre-Fortress-2/pf2/releases/latest -s | jq .tag_name -r > ./sdk/currentVersion.txt
 	download_game
 fi
 
